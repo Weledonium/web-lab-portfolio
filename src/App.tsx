dee645ee8dadf7c8f413c2a8ab6356265ca4f240
Atahan Bozkurt <atahanborab@gmail.com>
2026-02-24 10:15:55 +0300

    feat: add personal details to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
       <h1>Web Tasarimi ve Programlama</h1>
       <h2>LAB</h2>
       <p>Ad Soyad: Atahan Bora Bozkurt</p>
-      <p>Ogrenci No: 230541151</p>
+      <p>Öğrenci No: 230541151</p>
+      <p>Bölüm: Yazılım Mühendisliği</p>
+      <p>Hobiler: Vücut geliştirme</p>
     </div>
   )
 }
