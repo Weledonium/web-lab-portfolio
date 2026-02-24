184ab922d8daae001130efaf4fe04d7485b68574
Atahan Bozkurt <atahanborab@gmail.com>
2026-02-24 10:09:07 +0300

    chore: initial portfolio setup with Vite React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB</h2>
+      <p>Ad Soyad: Atahan Bora Bozkurt</p>
+      <p>Ogrenci No: 230541151</p>
+    </div>
+  )
+}
+
+export default App
